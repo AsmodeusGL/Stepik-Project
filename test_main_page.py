ca939cd21b6573458874f93bc25ad519560cd756
asmodeusgl <p.kruglikovskii@gmail.com>
2022-09-26 13:59:08 +0300

    Created a first Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -5,9 +5,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+
+
 class TestMain:
     def test_guest_can_go_to_login_page(self, browser):
         link = "https://selenium1py.pythonanywhere.com/"
         browser.get(link)
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        go_to_login_page(browser)
+
+    
