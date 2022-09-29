5d72ef567fe1c1211eb96635e198125a91664b91
AsmodeusGL <76130823+AsmodeusGL@users.noreply.github.com>
2022-09-29 21:37:23 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -32,7 +32,7 @@ class TestUserAddToBasketFromProductPage:
 
 
 @pytest.mark.need_review
-def test_quest_can_add_product_to_basket(browser):
+def test_guest_can_add_product_to_basket(browser):
     link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.open()
