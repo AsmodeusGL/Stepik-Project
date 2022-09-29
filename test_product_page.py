b49ae5d0331d9593d2d10a738d07c826f78e5ac2
AsmodeusGL <76130823+AsmodeusGL@users.noreply.github.com>
2022-09-29 21:36:25 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -16,13 +16,13 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.register_new_user(f'{time.time()}@fakemail.org', '1664372877')
 
-    @pytest.mark.need_review
     def test_user_cant_see_success_message(self, browser):
         link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = LoginPage(browser, link)
         page.open()
         page.should_be_authorized_user()
-
+        
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = LoginPage(browser, link)
